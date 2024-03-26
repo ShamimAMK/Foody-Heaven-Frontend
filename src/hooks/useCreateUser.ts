2a7b211b7a61086bbf4b0f6aa83e445ac6893b24
Shamim Al Mamun <smkhan.info@gmail.com>
2024-03-26 11:38:27 +0600

    Auth call back

--- src/hooks/useCreateUser.ts
+++ src/hooks/useCreateUser.ts
@@ -1,6 +1,5 @@
-"use client";
-
 import { API_BASE_URL } from "@/constants";
+import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation } from "react-query";
 
 type createUserType = {
@@ -9,10 +8,15 @@ type createUserType = {
 };
 
 export const useCreateUser = () => {
+	const { getAccessTokenSilently } = useAuth0();
+
 	const createMyUserRequest = async (user: createUserType) => {
+		const accessToken = await getAccessTokenSilently();
+
 		const res = await fetch(`${API_BASE_URL}/api/my/user`, {
 			method: "POST",
 			headers: {
+				Authorization: `Bearer ${accessToken}`,
 				"Content-Type": "application/json",
 			},
 			body: JSON.stringify(user),
--- src/provider/MyAuth0Provider.tsx
+++ src/provider/MyAuth0Provider.tsx
@@ -2,7 +2,7 @@
 
 import { useCreateUser } from "@/hooks/useCreateUser";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
-import { Mail } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { PropsWithChildren } from "react";
 
 const MyAuth0Provider = ({ children }: PropsWithChildren) => {
@@ -10,15 +10,14 @@ const MyAuth0Provider = ({ children }: PropsWithChildren) => {
 	const clientId = process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID;
 	const callbackUri = process.env.NEXT_PUBLIC_AUTH0_CALLBACK_URI;
 
-	const { createUser } = useCreateUser();
+	const router = useRouter();
 
 	if (!domain || !clientId || !callbackUri) {
 		throw new Error("Unable to initialize auth");
 	}
-	const onRedirectCallback = (appState?: AppState, user?: User) => {
-		if (user?.sub && user?.email) {
-			createUser({ auth0Id: user.sub, email: user.email });
-		}
+
+	const onRedirectCallback = () => {
+		router.push("/auth-callback");
 	};
 
 	return (
